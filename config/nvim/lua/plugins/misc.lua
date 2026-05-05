a78b57b5a3fa359c29fc42423f2847146b06a20b
soiderino <57572890+soiderino@users.noreply.github.com>
2026-05-05 23:44:18 +0300

    Delete config/nvim/lua directory

--- config/nvim/lua/plugins/misc.lua
+++
@@ -1,9 +0,0 @@
-return {
-  'vyfor/cord.nvim',
-  build = ':Cord update',
-  opts = {
-    idle = {
-      timeout = 120000, -- 2 minutes
-    }
-  }
-}
--- config/nvim/lua/plugins/themes.lua
+++
@@ -1,7 +0,0 @@
-return {
-  'Shatur/neovim-ayu',
-  config = function()
-    vim.cmd.colorscheme('ayu')
-    vim.cmd([[ highlight Normal guifg=NONE guibg=NONE ]])
-  end,
-}
